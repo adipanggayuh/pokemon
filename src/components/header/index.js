a9c3e99083cd25593619dba1f1b3dd8d723cebba
mkikiadipanggayuh <adiepanggayuh@gmail.com>
2022-02-19 11:42:25 +0800

    fix header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,41 +1,65 @@
 import * as React from 'react';
+import logo from '../../media/logopoke.png';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import logo from '../../media/logopoke.png'
-import Grid from '@mui/material/Grid';
-import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
-import { Badge } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+
 const Header = () => {
-    const pokedex = useSelector(store => store.pokedex);
 
     return (
+
         <AppBar position="static">
-            <Container maxWidth="lg">
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Typography
-                            noWrap
-                            component="div"
-                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                        >
-                            <Link to="/">
-                                <img src={logo} alt='' height={70} />
-                            </Link>
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={6} textAlign={'right'}>
-                        <Link to="/pokedex">
-                            <Badge badgeContent={pokedex.length} color="secondary">
-                                <CatchingPokemonIcon color="action" />
-                            </Badge>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        <Link to="/">
+                            <img src={logo} alt='' height={70} />
                         </Link>
-                    </Grid>
-                </Grid>
+                    </Typography>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                    >
+                        <img src={logo} alt='' height={70} />
+                    </Typography>
+
+                </Toolbar>
             </Container>
         </AppBar>
+        // <AppBar position="static">
+        //     <Container maxWidth="lg">
+        //         <Grid container spacing={2}>
+        //             <Grid item xs={6}>
+        //                 <Typography
+        //                     noWrap
+        //                     component="div"
+        //                     sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+        //                 >
+        //                     <Link to="/">
+        //                         <img src={logo} alt='' height={70} />
+        //                     </Link>
+        //                 </Typography>
+        //             </Grid>
+        //             <Grid item xs={6} textAlign={'right'}>
+        //                 <Link to="/pokedex">
+        //                     <Badge badgeContent={pokedex.length} color="secondary">
+        //                         <CatchingPokemonIcon color="action" />
+        //                     </Badge>
+        //                 </Link>
+        //             </Grid>
+        //         </Grid>
+        //     </Container>
+        // </AppBar>
     );
 };
 export default Header;
